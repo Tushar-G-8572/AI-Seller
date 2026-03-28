67c4433dd723d0761e7582ebafe18366ec5e8537
Tushar Gupta <tusharg.mp1@gmail.com>
2026-03-28 13:54:11 +0530

    Create folder structure and user contoller and models

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+
+export async function authMiddleware(req, res, next) {
+    try {
+        const token =
+            req.cookies?.token ||
+            req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Authentication required"
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded.id
+
+        next();
+
+    } catch (err) {
+        console.error("Auth Middleware Error:", err);
+
+        if (err.name === "JsonWebTokenError") {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token"
+            });
+        }
+
+        if (err.name === "TokenExpiredError") {
+            return res.status(401).json({
+                success: false,
+                message: "Token expired"
+            });
+        }
+
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+}
