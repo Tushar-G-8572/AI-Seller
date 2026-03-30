c29546d8f8e6b95064d48f967ebdc9a097f3556b
Tushar Gupta <tusharg.mp1@gamil.com>
2026-03-30 17:04:37 +0530

    Added Dis t folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,19 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import productRouter from './routes/products.route.js';
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname,'..','public')));
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -17,6 +24,10 @@ app.use(cors({
 
 app.use('/api/auth',authRouter);
 app.use('/api/products',productRouter);
+
+app.use((req,res)=>{
+   res.sendFile(path.join(__dirname,'..','public','index.html'));
+});
 
 export default app;
 
